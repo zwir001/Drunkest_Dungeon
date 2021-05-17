36f34572714041073fb1b54a85ff627e194ad204
zwir001 <72789237+zwir001@users.noreply.github.com>
2021-05-17 19:00:22 +0200

    Add files via upload

---
+++ src/Dziekanat.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../Human/Student/Student.hpp"
+#include "../Human/Teacher/Teacher.hpp"
+#include "../Location/Location.hpp"
+#include "../Loot/Biwo/Biwo.hpp"
+#include "../Loot/Relic/Relic.hpp"
+
+class Dziekanat
+{
+    public:
+    Teacher Enemies[5];
+    Student Students[3];
+    Location Locations[3];
+    Biwo Biwo;
+    Relic Relic[4];
+
+    Dziekanat( int i);
+
+    Teacher get_enemy( int index );
+
+    void simulation();
+};
